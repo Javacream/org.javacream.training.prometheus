dbb6b8eae16846eba7f525179ab15ebc63cd0b6c
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-02-22 10:19:23 +0100

    add prometheus

--- application/java/org.javacream.training.rest.books.server/src/main/java/org/javacream/books/isbngenerator/web/WebIsbnGenerator.java
+++ application/java/org.javacream.training.rest.books.server/src/main/java/org/javacream/books/isbngenerator/web/WebIsbnGenerator.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.annotation.Timed;
+
 @RestController
 @RequestMapping(path = "api")
 @CrossOrigin
@@ -15,6 +17,7 @@ public class WebIsbnGenerator {
 	@Autowired
 	IsbnGenerator isbnGenerator;
 
+	@Timed(value = "isbngenerator.next.time", description = "Time taken to create an isbn")
 	@PostMapping(path = "isbn", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String nextIsbn() {
 		return isbnGenerator.next();
