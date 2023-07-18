46b6c8880d95ac48e0a161bec6ed5e6452edc39f
Rainer Sawitzki <training@rainer-sawitzki.de>
2023-07-18 14:58:06 +0200

    add TracingAspect

---
+++ application/java/org.javacream.training.rest.books.server/src/main/java/org/javacream/util/aspect/TracingAspect.java
@@ -0,0 +1,27 @@
+package org.javacream.util.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TracingAspect {
+
+	@Around("execution(* org.javacream..impl.*.*(..))")
+	public Object trace(ProceedingJoinPoint pjp) throws Throwable {
+		MethodSignature signature = (MethodSignature) pjp.getSignature();
+		String methodName = signature.getMethod().getName();
+		System.out.println("Entering " + methodName);
+		try {
+			Object result = pjp.proceed();
+			System.out.println("returning from " + methodName);
+			return result;
+		} catch (Throwable t) {
+			System.out.println("returning from " + methodName);
+			throw t;
+		}
+	}
+}
