0e5895dc03f3df97473e81b7d27b3a459ee332b9
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2024-09-16 16:33:28 +0200

    Initial (#1)
    * add java project
    
    * Update README.md
    
    * add docker
    
    * add TracingAspect
    
    * add annotation based pointcut
    
    * start custom JMX
    
    * fix application properties
    
    * implement simple RequestCounter
    
    * add micrometer and prometheus endpoint
    
    ---------
    
    Co-authored-by: Rainer Sawitzki <training@rainer-sawitzki.de>

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # org.javacream.training.prometheus
+
+## Organisatorisches
+
+* Referent Rainer Sawitzki, rainer.sawitzki@gmail.com
+
+* [Digitales Flipchart](https://docs.google.com/presentation/d/1oP7CQBaUCid4dLlSJgvvfmZvxva18uvy-qSCOe5rWpw/edit?usp=sharing)
+
+* Seminarzeiten
+  * 8 Unterrichtseinheiten mit jeweils 90 Minuten
+  * jeweils 9:00 - 16:15
+  * Mittagspause 45’ (12:15-13:00)
+  * Kaffeepausen 10:30 - 10:45 und 14:30 - 14:45
+
+  
---
+++ application/java/org.javacream.training.rest.books.server/src/main/java/org/javacream/books/isbngenerator/web/WebIsbnGenerator.java
@@ -0,0 +1,24 @@
+package org.javacream.books.isbngenerator.web;
+
+import org.javacream.books.isbngenerator.api.IsbnGenerator;
+import org.javacream.util.aspect.CountRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api")
+@CrossOrigin
+
+public class WebIsbnGenerator {
+	@Autowired
+	IsbnGenerator isbnGenerator;
+	@PostMapping(path = "isbn", produces = MediaType.TEXT_PLAIN_VALUE)
+	@CountRequest
+	public String nextIsbn() {
+		return isbnGenerator.next();
+	}
+}
