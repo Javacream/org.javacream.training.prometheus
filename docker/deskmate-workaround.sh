ba94025cb2dd79395e619ff563aa9a125882383d
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-03-28 07:19:10 +0100

    Workaround for Deskmates

--- docker/deskmate-workaround.sh
+++ docker/deskmate-workaround.sh
@@ -1,5 +1,1 @@
 sudo setfacl --modify user:sl01:rw /var/run/docker.sock
-sudo aa-remove-unknown
-sudo apparmor_parser -r /etc/apparmor.d/*snap-confine*
-sudo apparmor_parser -r /var/lib/snapd/apparmor/profiles/snap-confine*
-sudo apparmor_parser -r /var/lib/snapd/apparmor/profiles/snap.docker.compose
